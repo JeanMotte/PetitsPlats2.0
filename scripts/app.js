659f93f695b697aaf3ec90bd9aeb43395240d1e4
Jean Motte <jijimotte@gmail.com>
2025-01-19 22:29:40 +0100

    UPDATE

--- scripts/app.js
+++ scripts/app.js
@@ -20,13 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
     // Populate dropdowns
     const ingredientList = recipes.flatMap(recipe => recipe.ingredients);
     const ingredientItems = new Set(ingredientList.map(ingredient => ingredient.ingredient));
-    populateDropdown('ingredient_list', ingredientItems);
+    const finalIngredientItems = [...ingredientItems].map(ingredient => ingredient.charAt(0).toUpperCase() + ingredient.slice(1));
+    finalIngredientItems.sort();
+    populateDropdown('ingredient_list', finalIngredientItems);
 
     const applianceList = recipes.map(recipe => recipe.appliance);
     const applianceItems = new Set(applianceList);
-    populateDropdown('appliance_list', applianceItems);
+    const finalApplianceItems = [...applianceItems].map(appliance => appliance.charAt(0).toUpperCase() + appliance.slice(1));
+    finalApplianceItems.sort();
+    populateDropdown('appliance_list', finalApplianceItems);
 
     const ustensilsList = recipes.flatMap(recipe => recipe.ustensils);
     const ustensilsItems = new Set(ustensilsList);
-    populateDropdown('ustensil_list', ustensilsItems);
+    const finalUstensilsItems = [...ustensilsItems].map(ustensil => ustensil.charAt(0).toUpperCase() + ustensil.slice(1));
+    finalUstensilsItems.sort();
+    populateDropdown('ustensil_list', finalUstensilsItems);
 })
