e4a66fa3eb116f07a564db3f69729013f2dd7035
Jean Motte <jijimotte@gmail.com>
2025-01-19 17:24:16 +0100

    UPDATE: card style

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const recipeCard = document.createElement('article');
         recipeCard.classList.add('recipe-card');
         recipeCard.innerHTML = `
-        <img src="${recipe.image}" alt="${recipe.name}" />
-        <h2>${recipe.name}</h2>
-        <p>${recipe.description}</p>
-        `;
+        <div class="recipe-img-container">
+            <img src="../assets/recipes-assets/${recipe.image}" alt="${recipe.name}" />
+            <p class="recipe-time">${recipe.time} min</p>
+        </div>
+       <div class="recipe-content">
+            <h2 class="recipe-name">${recipe.name}</h2>
+            <p class="recette-label">Recette</p>
+            <p class="recette-description text-card">${recipe.description}</p>
+            <p class="recette-label">Ingredients</p>
+            <ul class="recette-ingredients">
+                ${recipe.ingredients.map(ingredient => `<li><p class="text-card">${ingredient.ingredient}</p><p>${ingredient.quantity} ${ingredient.unit}</p></li>`).join('')}
+        </div>
+            `
         recipesContainer.appendChild(recipeCard);
     });
 })
