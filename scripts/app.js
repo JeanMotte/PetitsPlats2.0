20642ab2c0a60510269549865cf5a6fee8fc4b32
Jean Motte <jijimotte@gmail.com>
2025-01-19 18:08:54 +0100

    UPDATE

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,13 @@
 import { recipes } from '../data/recipes.js';
+import { recipeAttributes, recipeTemplate } from './template/recipe-template.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const recipesContainer = document.querySelector('.recipes-container');
     recipes.forEach(recipe => {
         const recipeCard = document.createElement('article');
         recipeCard.classList.add('recipe-card');
-        recipeCard.innerHTML = `
-        <div class="recipe-img-container">
-            <img src="../assets/recipes-assets/${recipe.image}" alt="${recipe.name}" />
-            <p class="recipe-time">${recipe.time} min</p>
-        </div>
-       <div class="recipe-content">
-            <h2 class="recipe-name">${recipe.name}</h2>
-            <p class="recette-label">Recette</p>
-            <p class="recette-description text-card">${recipe.description}</p>
-            <p class="recette-label">Ingredients</p>
-            <ul class="recette-ingredients">
-                ${recipe.ingredients.map(ingredient =>
-            `<li>
-                <p class="text-card">${ingredient.ingredient ?? ''}</p>
-                <p class="recipe-quantity">${ingredient.quantity ?? ''} ${ingredient.unit ?? ''}</p>
-            </li>`).join('')}
-        </div>
-            `
+        recipeCard.innerHTML = recipeTemplate(recipe);
+
         recipesContainer.appendChild(recipeCard);
     });
 })
---
+++ scripts/template/recipe-template.js
@@ -0,0 +1,28 @@
+export const recipeTemplate = (recipe) => {
+    return `
+        <div class="recipe-img-container">
+            <img src="../assets/recipes-assets/${recipe.image}" alt="${recipe.name}" />
+            <p class="recipe-time">${recipe.time} min</p>
+        </div>
+        <div class="recipe-content">
+            <h2 class="recipe-name">${recipe.name}</h2>
+            <p class="recette-label">Recette</p>
+            <p class="recette-description text-card">${recipe.description}</p>
+            <p class="recette-label">Ingredients</p>
+            <ul class="recette-ingredients">
+                ${recipe.ingredients.map(ingredient =>
+        `<li>
+                <p class="text-card">${ingredient.ingredient ?? ''}</p>
+                <p class="recipe-quantity">${ingredient.quantity ?? ''} ${ingredient.unit ?? ''}</p>
+        </li>`).join('')}
+        </div>
+            `
+}
+
+export const recipeAttributes = (recipeCard) => {
+    const { ingredients, appliance, ustensils } = recipeCard.recipe;
+
+    recipeCard.setAttribute('data-ingredients', ingredients.map(i => i.ingredient).join(','));
+    recipeCard.setAttribute('data-appliance', appliance);
+    recipeCard.setAttribute('data-ustentils', ustensils.join(','));
+}
