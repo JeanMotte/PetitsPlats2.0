00764e39f62a387bf56e9693ba2b64b10f63773f
Jean Motte <jijimotte@gmail.com>
2025-01-20 22:45:38 +0100

    UPDATE

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -2,7 +2,8 @@ export const populateDropdown = (dropdown, items) => {
     const list = document.getElementById(dropdown)
     items.forEach(item => {
         const option = document.createElement('li')
-        option.textContent = item
+        option.innerHTML = `
+        <button class="dropdown_item">${item}</button>`
         list.appendChild(option)
     })
 }
@@ -12,16 +13,19 @@ const ingredientButton = document.getElementById('ingredient_button')
 const dropdownContentIngredients = document.querySelector('.dropdown_content_ingredient')
 ingredientButton.addEventListener('click', () => {
     dropdownContentIngredients.classList.toggle('active')
+    ingredientButton.classList.toggle('dropdown-open')
 })
 
 const applianceButton = document.getElementById('appliance_button')
 const dropdownContentAppliances = document.querySelector('.dropdown_content_appliance')
 applianceButton.addEventListener('click', () => {
     dropdownContentAppliances.classList.toggle('active')
+    applianceButton.classList.toggle('dropdown-open')
 })
 
 const ustensilsButton = document.getElementById('ustensil_button')
 const dropdownContentUstensils = document.querySelector('.dropdown_content_ustensil')
 ustensilsButton.addEventListener('click', () => {
     dropdownContentUstensils.classList.toggle('active')
+    ustensilsButton.classList.toggle('dropdown-open')
 })
