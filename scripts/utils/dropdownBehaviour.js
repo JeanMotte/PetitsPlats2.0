d4947ed85159c71e9f72082dddca806fb0a24214
Jean Motte <jijimotte@gmail.com>
2025-01-24 11:16:34 +0100

    UPDATE: chevron when dropdown opening

--- scripts/utils/dropdownBehaviour.js
+++ scripts/utils/dropdownBehaviour.js
@@ -15,9 +15,18 @@ const ustensilsButton = document.getElementById('ustensil_button')
 const dropdownsButtons = [ingredientButton, applianceButton, ustensilsButton]
 
 export const handleDropdowns = () => dropdownsButtons.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
         button.classList.toggle('dropdown-open')
         button.nextElementSibling.classList.toggle('active')
+        const chevron = button.querySelector('.chevron-down')
+        if (chevron) {
+            if (button.classList.contains('dropdown-open')) {
+                chevron.src = './assets/chevron-up.svg';
+            } else {
+                chevron.src = './assets/chevron-down.svg';
+            }
+        }
+
 
         // prevent multiple dropdowns simultaneously open
         dropdownsButtons.filter(btn => btn !== button).forEach(btn => {
